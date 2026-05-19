1f198b6afc0c817d244751691e5f0585497cc93a
keilaemanuelly62-png <keilaemanuelly62@gmail.com>
2026-05-19 08:22:52 -0300

    correção js

---
+++ codigo_pag_inicio/index.js
@@ -0,0 +1,11 @@
+const botao =
+ document.getElementById("iniciar");
+const mensagem =
+ document.getElementById("mensagem");
+
+botao.addEventListener("click" , function(){
+
+    mensagem.innerText = "você clicou no botao!";
+    window.location.href = "perguntas.html";
+     
+})
