82187795d2e2d84241e0bc6781c9c990f88596f3
Amanda_Dev <albuquerque.amanda0024@gmail.com>
2026-06-30 20:34:04 -0300

    Amanda
    eu Amanda corrigi códigos do JavaScript em relação ao botão das perguntas "próximo"

--- codigo_pag_inicio/index.js
+++ codigo_pag_inicio/index.js
@@ -1,12 +1,50 @@
 
-const botao =
- document.getElementById("iniciar");
-const mensagem =
- document.getElementById("mensagem");
+const paginas = document.querySelectorAll('.pagina');
+const btnAnterior = document.getElementById('btnAnterior');
+const btnProximo = document.getElementById('btnProximo');
 
-botao.addEventListener("click" , function(){
+const indicadorPagina = document.getElementById('indicadorPagina');
+let paginaAtual = 0;
+function atualizarPagina(){
+    
+    paginas.forEach(pagina=> {
+    pagina.style.display = 'none';
+    
+    });    
 
-    mensagem.innerText = "você clicou no botao!";
-    window.location.href = "perguntas.html";
-     
-})
+    paginas[paginaAtual].style.display = 'block';
+    indicadorPagina.textContent = 
+    'Página ${paginaAtual + 1} de ${paginas.length}';
+    
+    btnAnterior.disabled =(paginaAtual === 0);
+    btnProximo.disabled = (paginaAtual === paginas.length - 1);
+  }   
+
+    btnProximo.addEventListener ( "click", ()  => {
+
+      if (paginaAtual < paginas.length - 1){
+        
+        paginaAtual ++ ;
+        atualizarPagina();
+      }
+      
+    });
+   
+     btnAnterior.addEventListener (  "click" , ()  => 
+     {
+
+      if (paginaAtual > 0) {
+         paginaAtual--;
+         atualizarPagina();
+
+
+      }
+
+      });
+
+
+
+      
+
+
+    
